8e9c6df49b68c011111d06b9126cfd464845f080
brungran <brungran@gmail.com>
2022-10-12 07:06:55 -0300

    files

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\mainController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/* Route::get('/', function () {
+    return view('welcome');
+}); */
+Route::get('/', [mainController::class, 'main'])->name('site.index');
+Route::get('/about', [AboutController::class, 'main'])->name('site.about');
+Route::get('/contact', [ContactController::class, 'main'])->name('site.contact');
+/* Route::get('/contact/{name}{surname}', function ($name, $surname) {
+    return $name . ' ' . $surname;
+}); */
+
+//prefixes
+Route::prefix('app')->group(function(){
+    Route::get('/clients', function () {
+        return 'clients';
+    })->name('app.clients');
+    Route::get('/providers', function () {
+        return 'providers';
+    })->name('app.providers');
+    Route::get('/products', function () {
+        return 'products';
+    })->name('app.products');
+});
