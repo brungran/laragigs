1a5921fd58e47bb2dc2849f8c9c3e3a987636e15
brungran <brungran@gmail.com>
2022-10-14 16:45:38 -0300

    route naming convention reminder

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,12 @@
 <?php
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing  
 
 use App\Http\Controllers\mainController;
 use App\Http\Controllers\AboutController;
